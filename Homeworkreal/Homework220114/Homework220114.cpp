307c587b3e9d5b243be96677b628366e751aef6f
허동석 <zest2700@gmail.com>
2022-01-17 01:34:49 +0900

    220114 HomeWork

---
+++ Homeworkreal/Homework220114/Homework220114.cpp
@@ -0,0 +1,38 @@
+﻿// Homework220114.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+//ㅠㅠ
+#include <iostream>
+
+
+void ToUpper(const char* _Left, char* _Right)
+{
+	int Value = 0;
+
+	while (0 < _Left[Value])
+	{
+
+		_Right[Value] = _Left[Value];
+
+			
+		Value += 1;
+
+			}
+	return ;
+
+}
+
+int main()
+{
+        char Text[100];
+
+		ToUpper("12abcceee", Text);
+		//      "12ABCCEEE"
+
+		
+		printf_s(Text);
+	
+		
+ 
+    
+}
+
